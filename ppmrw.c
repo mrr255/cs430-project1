b95843f9afa6cab7d9ed76a3a96f3ca6d0ac1ff4
Matthew <mrr255@nau.edu>
2016-09-14 20:51:16 -0700

    added Test file. Added parsing of header (up to image size).

--- ppmrw.c
+++ ppmrw.c
@@ -7,20 +7,74 @@ int main(int argc, char *argv[]) {
 
   // check for appropriate arguments
   if (argc != 4) {
-    fprintf(stderr, "Usage: ppmrw typenum input.ppm output.ppm w h\n");
+    fprintf(stderr, "Usage: ppmrw typenum input.ppm output.ppm\n");
     return(1);
   }
 
   FILE* fh = fopen(argv[2], "r");
+  FILE* fw = fopen(argv[3], "w");
   int c = fgetc(fh);
   if (c != 'P')
     fprintf(stderr, "%s\n", "Error: file not of type ppm or header not formatted correctly.");
   c = fgetc(fh);
   if( c == '3')
-  printf("Type 3");
-  else if( c == '6')
-  printf("Type 6");
+  printf("Type 3\n");
+  else if( c == '6'){
+  printf("Type 6\n");
+  fprintf(fw, "P6\n");
+}
   else
     fprintf(stderr, "%s\n", "Error: file not of type ppm or header not formatted correctly.");
+
+  c = fgetc(fh);
+  printf("%c\n", c);
+  if (c != '\n')
+    fprintf(stderr, "%s\n", "Error: file not of type ppm or header not formatted correctly.");
+  c = fgetc(fh);
+  int d;
+  while (c == '#'){
+    fprintf(fw,"%c", c);
+    d = fgetc(fh);
+    fprintf(fw,"%c", d);
+    while (d!= '\n'){
+      d = fgetc(fh);
+      fprintf(fw,"%c", d);
+    }
+      //fprintf(fw,"\n");
+      c = fgetc(fh);
+    }
+
+    printf("%s\n", "Height");
+    printf("%c\n", c);
+    char *height;
+    height = malloc(16 * 5);
+    int i = 0;
+    while(c != ' '){
+      height[i] = c;
+      c = fgetc(fh);
+      printf("%c\n", c);
+      i+=1;
+    }
+      int h = atoi(height);
+
+      printf("%s\n", "width");
+    char *width;
+    width = malloc(16 * 5);
+    i = 0;
+    while(c != '\n'){
+      width[i] = c;
+      c = fgetc(fh);
+      printf("%c\n", c);
+      i+=1;
+    }
+    int w = atoi(width);
+    printf("%i\n",h);
+    printf("%i\n",w);
+
+    fprintf(fw, "%i ", h);
+    fprintf(fw, "%i\n", w);
+
+
+
 return (0);
 }
